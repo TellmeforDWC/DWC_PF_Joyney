4b67df3b7bf59ff3743368f0f4837d208d7c05f6
TellmeforDWC <rikugame1003@gmail.com>
2021-11-24 12:40:17 +0000

    [update]管理者ログイン機能、検索機能、ページネーション実装

--- app/controllers/admin/spots_controller.rb
+++ app/controllers/admin/spots_controller.rb
@@ -1,9 +1,31 @@
 class Admin::SpotsController < ApplicationController
 
+  before_action :admin_authentication
+
   def index
-    @spots = Spot.all
+    @spots = Spot.all.page(params[:page]).per(10)
     @tags = @spots.tag_counts_on(:tags)
-    gon.spot = @spots
+  end
+
+  def search
+    # 検索フォームに何も入力しなかった場合
+    if params[:keyword].empty? && params[:tag].empty?
+      flash[:alert] = "検索条件を指定して下さい"
+      redirect_to admin_spots_path
+    # タグのみで検索する場合
+    elsif params[:keyword].empty?
+      @spot = Spot.tagged_with(params[:tag], :match_all => false).page(params[:page]).per(10)
+      @tags = Spot.all.tag_counts_on(:tags)
+    # 観光地名のみで検索する場合
+    elsif params[:tag].empty?
+      @spot = Spot.where("spot_name LIKE ?", "%#{params[:keyword]}%").page(params[:page]).per(10)
+      @tags = Spot.all.tag_counts_on(:tags)
+    # 観光地名とタグの両方で検索する場合
+    else
+      @spot = Spot.where("spot_name LIKE ?", "%#{params[:keyword]}%")
+      @spot = @spot.tagged_with(params[:tag], :match_all => false).page(params[:page]).per(10)
+      @tags = Spot.all.tag_counts_on(:tags)
+    end
   end
 
   def new
@@ -33,12 +55,21 @@ class Admin::SpotsController < ApplicationController
   end
 
   def destroy
+    @spot = Spot.find(params[:id])
+    @spot.destroy
+    redirect_to admin_spots_path
   end
 
   private
 
   def spot_params
     params.require(:spot).permit(:spot_name, :image, :longitude, :latitude, :tag_list)
+  end
+
+  def admin_authentication
+    unless admin_signed_in?
+      redirect_to choice1_spot_path
+    end
   end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    admin_spots_path # ログイン後に遷移するpathを設定
+  end
+
+  def after_sign_out_path_for(resource)
+    choice1_spot_path # ログアウト後に遷移するpathを設定
+  end
+
 end
